e11e048fca3e67da798cec70a85648d378c7acf2
Cody Switzer <cswitzer85@gmail.com>
2020-03-17 21:58:46 -0500

    updated db comments

--- public/db.js
+++ public/db.js
@@ -1,12 +1,12 @@
 let db;
-
+// request opens a budget database with version number 1
 const request = indexedDB.open("budget", 1);
-
+// if budget does not exist or version is lower than the above stated number, upgrade is needed and this will run
 request.onupgradeneeded = function(event) {
   const db = event.target.result;
   db.createObjectStore("pending", { autoIncrement: true });
 };
-
+// if budget is opened successfully this will run
 request.onsuccess = function(event) {
   db = event.target.result;
 
@@ -14,23 +14,23 @@ request.onsuccess = function(event) {
     checkDatabase();
   }
 };
-
+// if ther was an error in trying to open budget, the error code is logged to the console
 request.onerror = function(event) {
-  console.log("Woops! " + event.target.errorCode);
+  console.log("Oh, blunder! " + event.target.errorCode);
 };
-
+// this function saves the transaction data to the object store
 function saveRecord(record) {
   const transaction = db.transaction(["pending"], "readwrite");
   const store = transaction.objectStore("pending");
 
   store.add(record);
 }
-
+// this functino checks the db
 function checkDatabase() {
   const transaction = db.transaction(["pending"], "readwrite");
   const store = transaction.objectStore("pending");
   const getAll = store.getAll();
-
+// all data from the object store
   getAll.onsuccess = function() {
     if (getAll.result.length > 0) {
       fetch("/api/transaction/bulk", {
